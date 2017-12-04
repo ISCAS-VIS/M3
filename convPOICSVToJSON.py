6a19b1e9d82d84b0ae650df46dfeeeeff24225bd
Joe Jiang <hijiangtao@gmail.com>
2017-12-04 10:47:29 +0800

    update: POI res csv convert to json script

---
+++ convPOICSVToJSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# 将 uniPOIDistribution.py 中生成的结果转化成 JSON 格式并汇集到一个文件，准备存入 mongoDB
+
+import sys
+import time
+from util.csvToMatrixJson import csvToMatrixJson
+
+if __name__ == '__main__':
+	STARTTIME = time.time()
+	print "Start approach at %s" % STARTTIME
+
+	csvToMatrixJson({
+		'KEYS': [],
+		'DIRECTORY': '/enigma/tao.jiang/datasets/JingJinJi/records/bj-newvis-sg',
+		'FilePrefix': 'hares-j',
+		'INUM': 20
+	}).run()
+
+	print "END TIME: %s" % time.time()
---
+++ util/csvToMatrixJson.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Output Format:
+# [hares-x]
+# id, seg, gid, state, from_nid(default to 0), to_nid(default to 0)
+
+import logging
+import json
+import os
+from util.preprocess import getCityLocs, formatGridID, formatTime
+
+class csvToMatrixJson(object):
+	"""
+	Convert CSV file to JSON with given params
+		:param object: 
+	"""
+	def __init__(self, PROP):
+		super(csvToMatrixJson, self).__init__()
+
+		self.keys = PROP['keys']  # 暂未使用，通用性未考虑
+		self.INUM = PROP['INUM']
+		self.DIRECTORY = PROP['DIRECTORY']
+		self.FilePrefix = PROP['FilePrefix']
+
+	def run(self):
+		resArr = []
+		ofile = os.path.join(self.DIRECTORY, "%sat" % self.FilePrefix)
+		for x in xrange(0, self.INUM):
+			ifile = os.path.join(self.DIRECTORY, '%s%d' % (self.FilePrefix, x))
+
+			with open(ifile, 'rb') as stream:
+				for line in stream:
+					line = line.strip('\n')
+					linelist = line.split(',')
+					
+					# tmpObject, index = {}, 0
+					# for each in self.keys:
+					# 	tmpObject[each] = linelist[index]
+
+					resArr.append({
+						"pid": linelist[0],
+						"segid": int(linelist[1]),
+						"dev_num": int(linelist[2]),
+						"rec_num": int(linelist[3])
+					})
+			stream.close()
+		
+		with open(ofile, 'ab') as res:
+			json.dump(resArr, res)
+		res.close()
