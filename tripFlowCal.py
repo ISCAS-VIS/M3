b12fa578d690ed8f7790d91754847c10ddad255c
Joe Jiang <hijiangtao@gmail.com>
2018-02-01 11:53:25 +0800

    update: add params input entry for tripflow script

--- tripFlowCal.py
+++ tripFlowCal.py
@@ -59,13 +59,13 @@ min_samples	= %d
 
 def usage():
 	# /datahouse/zhtan/datasets/VIS-rawdata-region/
-	print "python tripFlowCal.py -d /datasets -p /datasets -e 0.01 -m 10"
+	print "python tripFlowCal.py -d /datasets -p /datasets -e 0.01 -m 10 -x 18"
 
 
 def main(argv):
 	try:
-		argsArray = ["help", 'stdindir=', 'stdoutdir', "eps", "min_samples"]
-		opts, args = getopt.getopt(argv, "hd:p:e:m:", argsArray)
+		argsArray = ["help", 'stdindir=', 'stdoutdir', "eps", "min_samples", "index="]
+		opts, args = getopt.getopt(argv, "hd:p:e:m:x:", argsArray)
 	except getopt.GetoptError as err:
 		print str(err)
 		usage()
@@ -74,6 +74,7 @@ def main(argv):
 	stdindir = '/home/tao.jiang/datasets/JingJinJi/records'
 	stdoutdir = '/home/tao.jiang/datasets/JingJinJi/records'
 	eps, min_samples = 0.01, 10
+	x = 9
 
 	for opt, arg in opts:
 		if opt == '-h':
@@ -87,6 +88,8 @@ def main(argv):
 			eps = float(arg)
 		elif opt in ('-m', '--min_samples'):
 			min_samples = int(arg)
+		elif opt in ('-x', '--index'):
+			x = int(arg)
 
 	STARTTIME = time.time()
 	print "Start approach at %s" % STARTTIME
@@ -99,7 +102,7 @@ def main(argv):
 	# min_samples	= %d
 	# ===	Cluster Opts	===
 	# ''' % (stdindir, stdoutdir, eps, min_samples)
-	x = 9
+
 	processTask(x, eps, min_samples, stdindir, stdoutdir)
 
 	# @多进程运行程序 END
