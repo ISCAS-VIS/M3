85d7add2c627ef6c8ed21eac6b6e9a9604ec4d6e
Joe Jiang <hijiangtao@gmail.com>
2018-03-09 10:48:44 +0800

    [2018-03-09 10:48:44] add Seeds Selection part, according to deviceNum Sorting result

--- SQL/clusterPbaseTable.sql
+++ SQL/clusterPbaseTable.sql
@@ -26,3 +26,12 @@ CREATE TABLE `stvis`.`tripflow` ( `id` INT NOT NULL AUTO_INCREMENT , `glng` DOUB
 ALTER TABLE `tripflow` ADD INDEX( `speed`, `rec_num`, `seg`);
 
 LOAD DATA LOCAL INFILE "/datahouse/tao.jiang/bj-byhour-res/mcres-9" INTO TABLE tripflow COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '' ESCAPED BY '"' LINES TERMINATED BY '\n' (@col1,@col2,@col3,@col4,@col5,@col6,@col7,@col8) set glng=@col1,glat=@col2,dir_type=@col3,speed=@col4,rec_num=@col5,dlng=@col6,dlat=@col7,seg=DATE_ADD("2016-07-05", INTERVAL @col8 HOUR);
+
+-- midTripFlow
+
+CREATE TABLE `stvis`.`midTripFlow` ( `id` INT NOT NULL AUTO_INCREMENT , `glng` DOUBLE NOT NULL , `glat` DOUBLE NOT NULL , `dir_type` CHAR(5) NOT NULL , `speed` DOUBLE NOT NULL , `rec_num` INT NOT NULL , `dlng` DOUBLE NOT NULL , `dlat` DOUBLE NOT NULL , `seg` TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+ALTER TABLE `midTripFlow` ADD INDEX( `speed`, `rec_num`, `seg`);
+
+LOAD DATA LOCAL INFILE "/datahouse/tao.jiang/bj-byhour-res/mcres-angle-9" INTO TABLE midTripFlow COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '' ESCAPED BY '"' LINES TERMINATED BY '\n' (@col1,@col2,@col3,@col4,@col5,@col6,@col7,@col8) set glng=@col1,glat=@col2,dir_type=@col3,speed=@col4,rec_num=@col5,dlng=@col6,dlat=@col7,seg=DATE_ADD("2016-07-05", INTERVAL @col8 HOUR);
+
